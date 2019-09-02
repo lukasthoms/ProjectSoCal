727cfd82ab9e308ca089c94fbf0293c262adc8a5
Lukas Thoms <luk@stho.ms>
2019-09-02 15:02:54 -0400

    Initial Commit

---
+++ ProjectSoCal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectSoCal
+//
+//  Created by Lukas Thoms on 9/2/19.
+//  Copyright © 2019 Lukas Thoms. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
